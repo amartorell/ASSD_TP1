8b8e7c06b4f06e8ee2dea8987d5da0cd8185db06
amartorell <amartorell@itba.edu.ar>
2019-03-26 19:23:00 -0300

    hice un poquitin mas fachera la GUI pero esta en graphs

--- graphs.py
+++ graphs.py
@@ -24,15 +24,13 @@ class GUI:
 
         self.ventana_derecha = Frame(self.root)
         self.ventana_derecha.grid(row=0,column=1)
-        self.label_test = Label(self.ventana_derecha,text="probando")
-        self.label_test.grid()
 
         self.ventana_inferior = Frame(self.root)
         self.ventana_inferior.grid(row=1)
 
 
         self.label_1 = Label(self.ventana_derecha,text="Caracteristicas de la señal:")
-        self.label_1.grid(row=0,column=0,columnspan=4)
+        self.label_1.grid(row=0,column=0,columnspan=2)
 
         self.label_2 = Label(self.ventana_derecha,text="Aca no se que poner")
         self.label_2.grid(row=5,column=0)
@@ -57,22 +55,30 @@ class GUI:
 
         self.check_faa = IntVar()
         self.CheckFAA = Checkbutton(self.ventana_derecha, text="FAA", variable=self.check_faa)
-        self.CheckFAA.grid(row=6,column=0,columnspan=2,padx=8,pady=8)
+        self.CheckFAA.grid(row=6,column=0)
 
         self.check_sample_and_hold = IntVar()
-        self.CheckSAH = Checkbutton(self.ventana_derecha, text="Sample and Hold", variable=self.check_sample_and_hold)
-        self.CheckSAH.grid(row=7,column=0,columnspan=2,padx=8,pady=8)
+        self.CheckSAH = Checkbutton(self.ventana_derecha, text="Sample and Hold", variable=self.check_sample_and_hold, command=self.mostrar_config)
+        self.CheckSAH.grid(row=6,column=2,columnspan=2,padx=8,pady=8)
+
+        self.Frecuencia_SH=Scale(self.ventana_derecha, from_=150, to=15000, label='Frecuencia S&h', orient=HORIZONTAL)
+        self.Frecuencia_SH.set(1000)
+
+        self.Duty_SH=Scale(self.ventana_derecha, from_=5, to=95, label='Duty cycle S&H', orient=HORIZONTAL)
+        self.Duty_SH.set(50)
 
         self.check_llave_analog = IntVar()
         self.CheckANKEY = Checkbutton(self.ventana_derecha, text="Llave analogica", variable=self.check_llave_analog)
-        self.CheckANKEY.grid(row=8,column=0,columnspan=2,padx=8,pady=8)
+        self.CheckANKEY.grid(row=6,column=4,columnspan=2,padx=8,pady=8)
 
         self.check_fr = IntVar()
         self.CheckFR = Checkbutton(self.ventana_derecha, text="FR", variable=self.check_fr)
-        self.CheckFR.grid(row=9,column=0,columnspan=2,padx=8,pady=8)
+        self.CheckFR.grid(row=6,column=6,columnspan=2,padx=8,pady=8)
 
         self.GraphButtom = Button(self.ventana_derecha, text="Graficar", command=self.Graficar)
-        self.GraphButtom.grid(row=10,column=0,columnspan=2)
+        self.GraphButtom.grid(row=10,column=6,columnspan=2)
+
+
 
 
 # ------------------------------------------------------------------------------------------------
@@ -95,6 +101,15 @@ class GUI:
 
     def Graficar(self):
         print("hacer algo wachin")
+
+
+    def mostrar_config(self):
+        if (self.check_sample_and_hold.get()):
+            self.Frecuencia_SH.grid(row=7, column=3, padx=8, pady=8)
+            self.Duty_SH.grid(row=8,column=3,padx=8,pady=8)
+        else:
+            self.Frecuencia_SH.grid_remove()
+            self.Duty_SH.grid_remove()
 
 
 b = GUI()
